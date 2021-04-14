e5b6ffc59ce83d2985a123925f27fa6068705937
decodeais <jenspeterschrade@gmail.com>
2021-04-14 11:11:14 +0200

    NMEA Compass works with listener with 250ms

--- app/src/main/java/io/github/tiagoshibata/gpsdclient/Compass.java
+++ app/src/main/java/io/github/tiagoshibata/gpsdclient/Compass.java
@@ -13,7 +13,7 @@ import java.util.Locale;
 //##########################################################################
 public class Compass implements SensorEventListener {
     private static final String TAG = "Compass";
-
+    private static long prevtime;
     public interface CompassListener {
         void onNewAzimuth(float azimuth);
     }
@@ -71,7 +71,8 @@ public class Compass implements SensorEventListener {
     @Override
     public void onSensorChanged(SensorEvent event) {
         final float alpha = 0.99f;
-        //long time = 0;
+
+        long actualtime = 0;
         synchronized (this) {
             if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
 
@@ -109,9 +110,11 @@ public class Compass implements SensorEventListener {
                 azimuth = (float) Math.toDegrees(orientation[0]); // orientation
                 azimuth = (-azimuth + azimuthFix + 360) % 360;
                 // Log.d(TAG, "azimuth (deg): " + azimuth);
-                System.currentTimeMillis();
-                if (listener != null) {
+             //   boolean b = 500 < ((actualtime - prevtime) & 0xEFFFFFFF);
+                actualtime = System.currentTimeMillis();
+                if ((listener != null ) & (250 < ((actualtime - prevtime) & 0xEFFFFFFF))){
                      listener.onNewAzimuth(azimuth);
+                    prevtime =actualtime;
                //     listener.onNewAzimuth(pitch);
                 }
             }
